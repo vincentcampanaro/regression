50b4e2a39627b7a0127b90b403d29d399c3535ca
Vincent Campanaro <124087048+vincentcampanaro@users.noreply.github.com>
2023-11-28 21:41:35 -0500

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function formatTime(seconds) {
     let minutes = Math.floor(seconds / 60);
-    let remainingSeconds = (seconds % 60).toFixed(2);
+    let remainingSeconds = Math.floor(seconds % 60);
     return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
 }
 
@@ -14,18 +14,20 @@ document.getElementById('swimmingForm').addEventListener('submit', function(e) {
     var gender = document.getElementById('gender').value;
     var userTimeInSeconds = parseFloat(document.getElementById('timeSlider').value);
 
-    // Example data for regression model
     const swimData = {
         'female': [25.69, 55.79, 2 * 60 + 0.89, 4 * 60 + 15.49],
         'male': [22.79, 49.99, 1 * 60 + 49.99, 3 * 60 + 55.59]
     };
 
-    // Simple regression calculation (for demo purposes)
     var predictedTime = swimData[gender].reduce((a, b) => a + b, 0) / swimData[gender].length;
 
-    // Creating the chart
     var ctx = document.getElementById('graph').getContext('2d');
-    var chart = new Chart(ctx, {
+
+    if (window.myChart) {
+        window.myChart.destroy();
+    }
+
+    window.myChart = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: ['Average Professional Time', 'Your Time'],
