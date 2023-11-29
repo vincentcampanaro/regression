cc079548cf3200fcaa69a89f3089a86875911b33
Vincent Campanaro <124087048+vincentcampanaro@users.noreply.github.com>
2023-11-28 21:34:32 -0500

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
 document.getElementById('swimmingForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
-    // Retrieve user input
     var gender = document.getElementById('gender').value;
-    var event = document.getElementById('event').value;
-    var time = document.getElementById('time').value;
+    var userTime = parseFloat(document.getElementById('time').value);
 
-    // TODO: Process the input, perform regression, and update the graph
-    // This part requires additional logic and possibly a data visualization library
+    // Example data for regression model
+    const swimData = {
+        'female': [25.69, 55.79, 2 * 60 + 0.89, 4 * 60 + 15.49],
+        'male': [22.79, 49.99, 1 * 60 + 49.99, 3 * 60 + 55.59]
+    };
+
+    // Simple regression calculation (for demo purposes)
+    var predictedTime = swimData[gender].reduce((a, b) => a + b, 0) / swimData[gender].length;
+
+    // Creating the chart
+    var ctx = document.getElementById('graph').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Average Professional Time', 'Your Time'],
+            datasets: [{
+                label: 'Time in seconds',
+                data: [predictedTime, userTime],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 });
-
-// TODO: Populate the events dropdown based on gender
