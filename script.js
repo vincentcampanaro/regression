54c03652bf3419c868bfad2d272a5bc9ce47f7f6
Vincent Campanaro <124087048+vincentcampanaro@users.noreply.github.com>
2023-11-28 21:16:13 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('swimmingForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Retrieve user input
+    var gender = document.getElementById('gender').value;
+    var event = document.getElementById('event').value;
+    var time = document.getElementById('time').value;
+
+    // TODO: Process the input, perform regression, and update the graph
+    // This part requires additional logic and possibly a data visualization library
+});
+
+// TODO: Populate the events dropdown based on gender
